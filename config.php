0279a2f680c9b657c4c71a2c679dccd437dcabcb
Andre-PC\Andre <andre.lichtenthaeler@gmail.com>
2018-01-29 18:50:29 +0100

    Added configs

--- config.php
+++ config.php
@@ -1,7 +1,11 @@
 <?php
+    //--Database
 	define("PREFIX", "cms_");
 	define("DB_HOST", "localhost");
 	define("DB_USER", "root");
 	define("DB_PASS", "");
 	define("DB_NAME", "cms");
+
+
+    define("URL", "http://localhost/Novus.io/");
 ?>
